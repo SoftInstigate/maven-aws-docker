93b1319c34409ebb77c2a3f86390b2808713e742
Maurizio Turatti <maurizio@softinstigate.com>
2019-02-21 18:20:52 +0100

    Fix wrong repo

--- bin/build.sh
+++ bin/build.sh
@@ -5,5 +5,5 @@ if [[ -z $ECS_CLI_RELEASE ]]; then
     echo "Error: undefined ECS_CLI_RELEASE environment variable."
     exit 1
 fi
-docker build --build-arg ECS_CLI_RELEASE="$ECS_CLI_RELEASE" -t softinstigate/serverless .
-docker tag softinstigate/serverless:latest softinstigate/serverless:"$ECS_CLI_RELEASE"
+docker build --build-arg ECS_CLI_RELEASE="$ECS_CLI_RELEASE" -t softinstigate/maven-aws .
+docker tag softinstigate/maven-aws:latest softinstigate/maven-aws:"$ECS_CLI_RELEASE"
--- bin/push.sh
+++ bin/push.sh
@@ -3,6 +3,6 @@ set -e
 
 if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
     docker login -u="$DOCKER_USER" -p="$DOCKER_PASS";
-    docker push softinstigate/serverless:latest;
-    docker push softinstigate/serverless:"$ECS_CLI_RELEASE";
+    docker push softinstigate/maven-aws:latest;
+    docker push softinstigate/maven-aws:"$ECS_CLI_RELEASE";
 fi
